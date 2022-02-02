ec23e2ae1f73bb797003c5146b5c04531eca0845
Leander Loew <leander.loew@connect-real.com>
2022-02-02 20:15:43 +0100

    added the slug in the registry as well

--- {{cookiecutter.project_name}}/deploy_dev.sh
+++ {{cookiecutter.project_name}}/deploy_dev.sh
@@ -1,11 +1,11 @@
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin {{cookiecutter.aws_id}}.dkr.ecr.eu-central-1.amazonaws.com
 
 docker build -t dev:latest -f compose/lambda_extended/Dockerfile .
-docker tag dev:latest {{cookiecutter.aws_id}}.dkr.ecr.eu-central-1.amazonaws.com/dev_lambda_extended:latest
-docker push {{cookiecutter.aws_id}}.dkr.ecr.eu-central-1.amazonaws.com/dev_lambda_extended:latest
+docker tag dev:latest {{cookiecutter.aws_id}}.dkr.ecr.eu-central-1.amazonaws.com/dev_{{cookiecutter.project_slug}}_lambda_extended:latest
+docker push {{cookiecutter.aws_id}}.dkr.ecr.eu-central-1.amazonaws.com/dev_{{cookiecutter.project_slug}}_lambda_extended:latest
 
 docker build -t dev:latest -f compose/lambda_base/Dockerfile .
-docker tag dev:latest {{cookiecutter.aws_id}}.dkr.ecr.eu-central-1.amazonaws.com/dev_lambda_base:latest
-docker push {{cookiecutter.aws_id}}.dkr.ecr.eu-central-1.amazonaws.com/dev_lambda_base:latest
+docker tag dev:latest {{cookiecutter.aws_id}}.dkr.ecr.eu-central-1.amazonaws.com/dev_{{cookiecutter.project_slug}}_lambda_base:latest
+docker push {{cookiecutter.aws_id}}.dkr.ecr.eu-central-1.amazonaws.com/dev_{{cookiecutter.project_slug}}_lambda_base:latest
 
 cd serverless_config && serverless deploy --stage dev
